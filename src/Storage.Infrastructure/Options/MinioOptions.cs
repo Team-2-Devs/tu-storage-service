3c51ae6ed39fbdbccaedfed0980ba5883c6e2c1a
Kenneth Haagen Sørensen <Haagen86@gmail.com>
2025-10-09 15:28:26 +0200

    feat(infra): add MinioOptions with validation

---
+++ src/Storage.Infrastructure/Options/MinioOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Infrastructure.Options;
+
+public sealed class MinioOptions
+{
+  [Required] public required string Endpoint { get; init; } = null!;
+  [Required] public required string AccessKey { get; init; } = null!;
+  [Required] public required string SecretKey { get; init; } = null!;
+  [Required] public required string BucketName { get; init; } = null!;
+
+  public bool UseSsl { get; init; } = true;
+  public string Region { get; init; } = "eu-central-1";
+  public string? PublicBaseUrl { get; init; }
+}
