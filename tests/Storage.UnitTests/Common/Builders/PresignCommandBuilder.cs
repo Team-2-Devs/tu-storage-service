83ce5b3373073e69db6ac1cc1384b987a4eb9adb
Kenneth Haagen Sørensen <Haagen86@gmail.com>
2025-11-09 15:15:02 +0100

    test(app): add PresignCommandBuilder

---
+++ tests/Storage.UnitTests/Common/Builders/PresignCommandBuilder.cs
@@ -0,0 +1,42 @@
+﻿using Storage.Application.Ports.Inbound.Contracts;
+
+namespace Storage.UnitTests.Common.Builders;
+
+/// <summary>Builder for presign-commands used in application layer unit tests.</summary>
+public sealed class PresignCommandBuilder
+{
+  private string _key = "images/2025/11/09/sample.jpg";
+  private int _ttl = 3600;
+  private string _contentType = "image/jpeg";
+
+  public PresignCommandBuilder WithKey(string key)
+  {
+    _key = key;
+    return this;
+  }
+
+  public PresignCommandBuilder WithTtl(int ttl)
+  {
+    _ttl = ttl;
+    return this;
+  }
+
+  public PresignCommandBuilder WithContentType(string contentType)
+  {
+    _contentType = contentType;
+    return this;
+  }
+  
+  public PresignGetUrlCommand BuildGet() =>
+    new(
+      Key: _key, 
+      TtlSec: _ttl
+    );
+
+  public PresignPutUrlCommand BuildPut() =>
+    new(
+      Key: _key,
+      ContentType: _contentType,
+      TtlSec: _ttl
+    );
+}
